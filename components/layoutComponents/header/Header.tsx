2dafb6c86cee8165a7d8f481b2b334ac1e4978af
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-05-26 06:16:09 +0200

    scroll_newApproach_2.2

--- components/layoutComponents/header/Header.tsx
+++ components/layoutComponents/header/Header.tsx
@@ -8,8 +8,8 @@ import { pagesLinks } from '@/data/basicData';
 export const Header = () => {
   /**JSX**/
   return (
-    <header className="fixed w-full top-0 left-0 right-0 h-[50px] z-[500] bg-dark">
-      <div className="h-fullw-full wrapper-1">
+    <header className="fixed w-full top-0 left-0 right-0 h-[50px] z-[500] ">
+      <div className="relative h-full w-full wrapper-1">
         <ul className="flex items-center justify-end h-full gap-6 ">
           {pagesLinks.map(link => (
             <li key={link.href}>
