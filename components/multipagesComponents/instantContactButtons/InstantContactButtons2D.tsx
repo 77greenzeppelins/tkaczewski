ddc00df9aeeef0d69d7f8886d30881f1f8795561
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-06-21 14:48:19 +0200

    PageHomeContent_2.1

--- components/multipagesComponents/instantContactButtons/InstantContactButtons2D.tsx
+++ components/multipagesComponents/instantContactButtons/InstantContactButtons2D.tsx
@@ -2,11 +2,24 @@
 import React from 'react';
 /**Hook**/
 import useWindowSize from '@/hooks/useWindowSize';
+/**Components**/
+import { DirectPhone, DirectEmail } from '@/components'; //'@/components'
 
-import DirectPhone from '@/components/multipagesComponents/_basicComponents/links/directContactLinks/DirectPhone';
-import DirectEmail from '@/components/multipagesComponents/_basicComponents/links/directContactLinks/DirectEmail';
+/**TS**/
+interface Props {
+  phoneContainerStyle?: string;
+  emailContainerStyle?: string;
 
-const InstantContactButtons2D = () => {
+  buttonWidth?: number;
+  buttonHeight?: number;
+}
+/**------------------------------------**/
+const InstantContactButtons2D = ({
+  phoneContainerStyle,
+  emailContainerStyle,
+  buttonWidth = 0.25,
+  buttonHeight = 0.3,
+}: Props) => {
   /*
   ___1. 3DObjects in canvas reacts (resizes) on viewPort height; this hook is required as these "buttons" have to follow 3DObjects size 
   */
@@ -16,32 +29,35 @@ const InstantContactButtons2D = () => {
 
   return (
     <div className="w-full h-full z-1">
-      <div className="flex justify-center h-[50%] w-full pt-[10vh]">
+      <div
+        className={`flex justify-center h-[50%] w-full ${
+          phoneContainerStyle ? phoneContainerStyle : 'pt-[10vh]'
+        } `}
+      >
         <div
           // className="pointer-events-auto"
           className="bg-corpo opacity-25"
-          style={{ width: height * 0.25, height: height * 0.3 }}
+          style={{ width: height * buttonWidth, height: height * buttonHeight }}
         >
           <DirectPhone aStyle={'block w-full h-full'} hasLabel={false} />
         </div>
       </div>
 
-      <div className="flex justify-center h-[50%] w-full pt-[10vh] ">
+      <div
+        className={`flex justify-center h-[50%] w-full ${
+          emailContainerStyle ? emailContainerStyle : 'pt-[10vh]'
+        } `}
+      >
         <div
           // onClick={() => {
           //   console.log('...mail');
           // }}
           className="bg-corpo opacity-25"
-          style={{ width: height * 0.25, height: height * 0.3 }}
+          style={{ width: height * buttonWidth, height: height * buttonHeight }}
         >
           <DirectEmail aStyle={'block w-full h-full'} hasLabel={false} />
         </div>
       </div>
-      {/* <animated.div
-        // style={scrollDrivenStyles}
-        style={{ opacity: opacity }}
-        className="absolute inset-0 bg-dark pointer-events-none"
-      /> */}
     </div>
   );
 };
