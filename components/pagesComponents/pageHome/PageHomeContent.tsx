a4b5e00d821aac388c527b1ae543cefb9e458a52
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-06-22 08:22:41 +0200

    pageContacts_layout_2.0

--- components/pagesComponents/pageHome/PageHomeContent.tsx
+++ components/pagesComponents/pageHome/PageHomeContent.tsx
@@ -5,18 +5,18 @@ import useScrollPosition from '@/hooks/useScrollPosition';
 import useMeasure from 'react-use-measure';
 /**Components*/
 import { InstantContactButtons2D } from '@/components';
-import { useSpring, animated } from '@react-spring/web';
+
 import { basicConfigs, springConfigs } from '@/data/basicData';
 /**----------------------------------------**/
 const PageHomeContent = () => {
   /*
-   ___1. mountingCondition takes its valu from: progress of scrolling === responsive viewport height minus responsive viewport height divided by hardCoded 7;
+  ___1. what is going here? we want to catch the moment when user scroll to the bottom of the document => scrolled the whole page => scrollYPosition === all viewPorts - 1 viewPort
+  ___2. mountingCondition takes its valu from: progress of scrolling === responsive viewport height minus responsive viewport height divided by hardCoded 7;
   */
   const scrollYPosition = useScrollPosition();
   const [ref, { height }] = useMeasure({ scroll: true });
   const muntingCondition =
     scrollYPosition.val === height - height / basicConfigs.pageHome.viewports;
-  // console.log('window.innerHeight:', window.innerHeight);
 
   // if (muntingCondition) {
   //   console.log('...show!');
@@ -24,31 +24,29 @@ const PageHomeContent = () => {
   //   console.log('...hide!');
   // }
 
-  const { scale } = useSpring({
-    scale: muntingCondition ? 1 : 0,
-    // config: springConfigs.heavyAndSlow,
-    // config: { delay: muntingCondition ? 1 : 0 },
-  });
-
   /**JSX**/
   return (
     <div ref={ref} data-container="PageHomeContent">
       <div className=" h-[600vh]" />
       <div className="flex justify-center items-center h-[100vh] w-full">
         {/* {muntingCondition ? <InstantContactButtons2D /> : null} */}
-        <animated.div
-          className="h-full"
+        <div
+          className={`h-full  ${
+            muntingCondition
+              ? 'scale-100 transition duration-1000 delay-1000'
+              : 'scale-0 transition duration-0 delay-0'
+          }`}
           //  {...spring}
-          style={{ scale: scale }}
+          // style={{ scale: scale }}
         >
           {' '}
           <InstantContactButtons2D
             phoneContainerStyle={'pt-[13vh]'}
             emailContainerStyle={'pt-[8vh]'}
-            // buttonWidth={0.25}
-            // buttonHeight={0.3}
+            buttonWidth={0.22}
+            buttonHeight={0.28}
           />
-        </animated.div>
+        </div>
       </div>
     </div>
   );
