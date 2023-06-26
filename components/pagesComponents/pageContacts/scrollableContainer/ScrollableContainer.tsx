0fad5450f266bc3da825b430df7b655e58cf3c4f
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-06-26 14:24:45 +0200

    next_headers_API_1.5.2

--- components/pagesComponents/pageContacts/scrollableContainer/ScrollableContainer.tsx
+++ components/pagesComponents/pageContacts/scrollableContainer/ScrollableContainer.tsx
@@ -1,4 +1,3 @@
-import { ContactsDataSection } from '@/components';
 import React from 'react';
 import PageContent from '../pageContent/PageContent';
 
@@ -10,7 +9,7 @@ const ScrollableContainer = ({ hintIsMobile }: Props) => {
   return hintIsMobile ? (
     <PageContent transform={undefined} hintIsMobile={hintIsMobile} />
   ) : (
-    <div className="relative w-[10px]">
+    <div className="relative w-[5px]">
       <div className="h-screen bg-yellow-600" />
       <div className="h-screen bg-green-700" />
       <div className="h-screen bg-blue-600" />
