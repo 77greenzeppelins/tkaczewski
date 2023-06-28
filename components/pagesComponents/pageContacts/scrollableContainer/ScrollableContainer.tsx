e8f490752c863139fe3de5de9ca9866eb4c2959a
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-06-28 05:30:34 +0200

    pageContacts_smoothScroll_3.0

--- components/pagesComponents/pageContacts/scrollableContainer/ScrollableContainer.tsx
+++ components/pagesComponents/pageContacts/scrollableContainer/ScrollableContainer.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PageContent from '../pageContent/PageContent';
 
+/**TS**/
 interface Props {
   hintIsMobile: boolean;
 }
 
+/**--------------------------------------------------------* */
 const ScrollableContainer = ({ hintIsMobile }: Props) => {
   /**JSX**/
   return hintIsMobile ? (
@@ -12,17 +14,17 @@ const ScrollableContainer = ({ hintIsMobile }: Props) => {
     ___1. rendered if mobile
     ___2. 
     */
-    <div data-container="ScrollableContainer-mobile">
+    <div
+      data-layout="ScrollableContainer-mobile"
+      //  className="relative"
+    >
       <PageContent
         hintIsMobile={hintIsMobile}
         //transform={undefined}
       />
     </div>
   ) : (
-    <div
-      data-container="ScrollableContainer-desktop"
-      className="relative w-[0px]"
-    >
+    <div data-layout="ScrollableContainer-desktop" className="relative w-[0px]">
       <div className="h-screen" />
       <div className="h-screen" />
       <div className="h-screen" />
