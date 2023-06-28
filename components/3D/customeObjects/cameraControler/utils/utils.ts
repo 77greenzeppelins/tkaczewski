5c10efddd1c799ae641e15839aee3a750245d1f2
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-06-28 09:09:48 +0200

    isFinished_1.3

--- components/3D/customeObjects/cameraControler/utils/utils.ts
+++ components/3D/customeObjects/cameraControler/utils/utils.ts
@@ -1,18 +1,8 @@
 import { pages3DPositions, pagesPath } from '@/data/basicData';
-/*
-const pages3DPositions = {
-  pageHome: { x: 0 },
-  page2D: { x: 10 },
-  pageCV: { x: 20 },
-  pageContacts: { x: 30 },
-};
-*/
 
 const setXPosition = (path: string) => {
   switch (path) {
     case pagesPath.homePath:
-      return pages3DPositions.pageHome.x;
-    case pagesPath.exp2d:
       return pages3DPositions.page2D.x;
     case pagesPath.cvPath:
       return pages3DPositions.pageCV.x;
