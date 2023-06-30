e32a5deceb32e3f8ba94e71ec1d70c6f9b7692e4
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-06-30 08:39:48 +0200

    headerLink_disabledClass_1.1

--- components/layoutComponents/header/headerLink/HeaderLink.tsx
+++ components/layoutComponents/header/headerLink/HeaderLink.tsx
@@ -26,25 +26,29 @@ const HeaderLink: React.FC<Props> = ({ label, url }) => {
       aria-label={`Link do strony ${label}`}
       aria-current={isActive ? 'page' : undefined}
       scroll={false}
-      className="relative disable"
+      // className="relative "
+      className={`relative text-light disable ${
+        isActive ? 'hover:text-light' : 'hover:text-corpo'
+      }  delay-100 duration-300 ease-linear`}
     >
       {url === path && (
         <motion.span
           // layoutId="underline" // don't cooperate with canvas....
-          className="absolute top-full block h-[1px] bg-corpo origin-center"
+          className="absolute top-full block h-[1px] bg-corpo origin-center "
           initial={{ width: 0 }}
           animate={{ width: '100%' }}
           transition={{ duration: 0.6, delay: 0.2 }}
         />
       )}
 
-      <p
-        className={`text-light ${
+      {/* <p
+        className={`text-light  ${
           isActive ? 'hover:text-light' : 'hover:text-corpo'
         }  delay-100 duration-300 ease-linear`}
       >
         {label}
-      </p>
+      </p> */}
+      {label}
     </Link>
   );
 };
