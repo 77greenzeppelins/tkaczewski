4e3af3ae03f9f94a2f8d24b4dd20aed15af082bd
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-07-25 06:28:40 +0200

    OverlayForResizing_scrollReset_1.0

--- components/layoutComponents/appContent/AppContainer.tsx
+++ components/layoutComponents/appContent/AppContainer.tsx
@@ -4,8 +4,6 @@ import { GlobalContextProvider } from '@/context/ContextProvider';
 /**Components**/
 import MainCanvas from '@/components/3D/3D_Canvas/MainCanvas';
 import CanvasOverlay from '../overlays/canvasOverlay/CanvasOverlay';
-// import IntroOverlay from '../overlays/introOverlay/IntroOverlay';
-// import EmergencyOverlay from '../overlays/emergencyOverlay/EmergencyOverlay';
 
 /**---------------------------------------------------------------------**/
 const AppContainer = ({ children }: { children: React.ReactNode }) => {
@@ -17,10 +15,8 @@ const AppContainer = ({ children }: { children: React.ReactNode }) => {
       className="relative z-[400]"
     >
       <GlobalContextProvider
-        // IntroOverlay={IntroOverlay}
         CanvasOverlay={CanvasOverlay}
         MainCanvas={MainCanvas}
-        // EmergencyOverlay={EmergencyOverlay}
       >
         {children}
       </GlobalContextProvider>
--- components/layoutComponents/overlays/forResizing/OverlayForResizing.tsx
+++ components/layoutComponents/overlays/forResizing/OverlayForResizing.tsx
@@ -8,25 +8,31 @@ export default function OverlayForResizing() {
   const { width, height } = useWindowSize();
   const val = (width / height).toString();
   const aR = width / height;
-  const debouncedValue = useDebounce<string>(val, 400);
+  const debouncedValue = useDebounce<string>(val, height * 2.9);
   /*
   ___1. make visible
   */
   useEffect(() => {
     setMakeVisible(true);
+    window.scrollTo(0, 0);
   }, [width, height]);
   /*
   ___1. make invisible
   */
   useEffect(() => {
+    // window.scrollTo(0, 0);
     setMakeVisible(false);
     // setCounter(val => val + 1);
   }, [debouncedValue]);
 
+  useEffect(() => {
+    console.log('aR:', aR);
+  }, [aR]);
+
   /**JSX**/
   return (
     <div
-      className={`fixed fc flex-col gap-y-4 z-[600] left-0 right-0 h-screen  backdrop-blur-xl  pointer-events-none 
+      className={`fixed fc flex-col gap-y-4 z-[600] left-0 right-0 h-screen  backdrop-blur-xl backdrop-grayscale pointer-events-none 
       ${makeVisible ? 'opacity-100' : 'opacity-0'}
       `}
     >
